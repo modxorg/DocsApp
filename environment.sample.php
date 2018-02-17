253410d561532d48de70b2be40fe9e182ab196cf
Mark Hamstra <hello@markhamstra.com>
2018-02-17 02:15:37 +0100

    Update sample from latest in use

--- environment.sample.php
+++ environment.sample.php
@@ -1,13 +1,10 @@
 <?php
 return [
     'settings' => [
+        'host' => 'docs.modx.local',
+        'docSources' => __DIR__ . '/doc-sources/',
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
 
         // Monolog settings
         'logger' => [
