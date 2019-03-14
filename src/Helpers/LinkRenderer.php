498386ffeed32a34428f883b896a54d9165a885e
Mark Hamstra <hello@markhamstra.com>
2019-03-14 02:27:00 +0100

    Process links to make sure they are valid and to highlight broken or external links.
    If links don't seem to point anywhere valid, check for a matching redirect. If no redirect exists, highlight as broken link.

--- src/Helpers/LinkRenderer.php
+++ src/Helpers/LinkRenderer.php
@@ -6,6 +6,7 @@ use League\CommonMark\HtmlElement;
 use League\CommonMark\Inline\Element\AbstractInline;
 use League\CommonMark\Inline\Element\Link;
 use League\CommonMark\Inline\Renderer\InlineRendererInterface;
+use MODXDocs\Views\NotFound;
 
 
 class LinkRenderer implements InlineRendererInterface
@@ -22,21 +23,35 @@ class LinkRenderer implements InlineRendererInterface
     public function render(AbstractInline $inline, ElementRendererInterface $htmlRenderer)
     {
         if (!($inline instanceof Link)) {
-            throw new \InvalidArgumentException('Incompatible inline type: ' . get_class($inline));
+            throw new \InvalidArgumentException('Incompatible inline type: ' . \get_class($inline));
         }
 
+        $href = $this->getHref($inline->getUrl());
         $attributes = [
-            'href' => $this->getHref($inline->getUrl())
+            'href' => $href,
         ];
 
-        if (isset($inline->attributes['title']) and strlen($inline->attributes['title']) > 0) {
+        if (isset($inline->attributes['title']) && $inline->attributes['title'] !== '') {
             $attributes['title'] = $htmlRenderer->escape($inline->data['title'], true);
         }
 
         if (static::isExternalUrl($inline->getUrl())) {
-            $attributes['class'] = 'external-link';
+            $attributes['class'] = 'link__external';
             $attributes['target'] = '_blank';
             $attributes['rel'] = 'noreferrer noopener';
+        }
+        else {
+            // Check if the link points to somewhere valid
+            $docs = getenv('DOCS_DIRECTORY');
+            if (!file_exists($docs . $href . '.md') && !file_exists($docs . $href . '/index.md')) {
+                $newUri = Redirector::findNewURI($href);
+                if ($newUri === false) {
+                    $attributes['class'] = 'link__broken';
+                }
+                else {
+                    $attributes['href'] = $newUri;
+                }
+            }
         }
 
         return new HtmlElement('a', $attributes, $htmlRenderer->renderInlines($inline->children()));
