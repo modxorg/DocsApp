14664d572b9065a3aac907b0e08b4458a93a1d2a
Mark Hamstra <hello@markhamstra.com>
2019-04-18 22:21:12 +0200

    Add a simple static cache service

---
+++ src/Services/CacheService.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace MODXDocs\Services;
+
+class CacheService
+{
+    private $cacheRoot;
+    private $enabled;
+
+    public function __construct()
+    {
+        $this->cacheRoot = rtrim(getenv('CACHE_DIRECTORY'), '/') . '/';
+        $this->enabled = (bool)getenv('CACHE_ENABLED');
+    }
+
+    public static function getInstance(): CacheService
+    {
+        return new self();
+    }
+
+    public function get($key, $hash = null)
+    {
+        if (!$this->enabled) {
+            return false;
+        }
+
+        $file = $this->keyToFile($key);
+        if (!file_exists($file)) {
+            return false;
+        }
+
+        $data = file_get_contents($file);
+        $data = json_decode($data);
+        if ($data instanceof \stdClass) {
+            if ($hash !== null && $data->hash !== $hash) {
+                return false;
+            }
+
+            if (is_numeric($data->expiration) && time() > $data->expiration) {
+                return false;
+            }
+
+            return $data->contents;
+        }
+
+        return false;
+    }
+
+    public function set($key, $value, $expiration = null, $hash = null)
+    {
+        if (!$this->enabled) {
+            return false;
+        }
+
+        $file = $this->keyToFile($key);
+        $data = [
+            'generated' => date('Y-m-d H:i:s'),
+            'contents' => $value,
+            'hash' => $hash,
+            'expiration' => $expiration,
+        ];
+
+        $this->ensurePathsExist(dirname($file));
+
+        file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
+        return true;
+    }
+
+    private function keyToFile($key)
+    {
+        return $this->cacheRoot . strtolower($key) . '.json';
+    }
+
+    private function ensurePathsExist(string $path): bool
+    {
+        if (file_exists($path) && is_dir($path)) {
+            return true;
+        }
+
+        if (!mkdir($path, 0777, true) && !is_dir($path)) {
+            return false;
+        }
+
+        return true;
+    }
+}
