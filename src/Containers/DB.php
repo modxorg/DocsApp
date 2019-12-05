581b447d86661cc3753980501cbbe19b7c36d585
Mark Hamstra <hello@markhamstra.com>
2019-12-06 00:42:27 +0100

    Simplify db container, so it's easier to add a temporary dump for the path

--- src/Containers/DB.php
+++ src/Containers/DB.php
@@ -9,8 +9,8 @@ class DB
     public static function load(ContainerInterface $container)
     {
         $container['db'] = function (ContainerInterface $container) {
-            $dir = getenv('BASE_DIRECTORY') . '/db/';
-            $db = new \PDO('sqlite:' . $dir . 'db.sqlite');
+            $dir = getenv('BASE_DIRECTORY') . 'db/db.sqlite';
+            $db = new \PDO('sqlite:' . $dir);
             $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             return $db;
         };
